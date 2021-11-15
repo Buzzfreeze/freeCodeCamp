f2c94e14a4fea5083cd04e44d3d814556bfb0b62
Oak <krissada@buzzfreeze.com>
2021-11-16 02:06:40 +0700

    Send to LRS only when has jwt

--- client/src/redux/ga-saga.js
+++ client/src/redux/ga-saga.js
@@ -11,42 +11,45 @@ function* callGaType({ payload: { type, data } }) {
   console.log(data);
   console.log('--');
   var jwt = window.getCookie('jwt_access_token');
-  var json = window.parseJwt(jwt);
-  var userId = json.accessToken.userId;
-  // GA1 page data /
-  // GA1 page data /learn/responsive-web-design/
-  //
-  if (type === 'page') {
-    var arr = data.split('/');
-    // show cer page
-    if (arr.length === 4 || arr.length === 3) {
-      var cerName = arr[2];
-      window.cerAttend(
-        userId + EMAIL_PREFIX,
-        userId,
-        LEARN_URL + data,
-        cerName,
-        cerName
-      );
-    } else if (arr.length === 5) {
-      // show challenge page
-      var moduleName = arr[3];
-      var challengeName = arr[4];
-      window.challengeAttend(
-        userId + EMAIL_PREFIX,
-        userId,
-        LEARN_URL + data,
-        challengeName,
-        challengeName
-      );
-      window.moduleAttend(
-        userId + EMAIL_PREFIX,
-        userId,
-        LEARN_URL + data,
-        moduleName,
-        moduleName
-      );
+  if (jwt !== '') {
+    var json = window.parseJwt(jwt);
+    var userId = json.accessToken.userId;
+    // GA1 page data /
+    // GA1 page data /learn/responsive-web-design/
+    //
+    if (type === 'page') {
+      var arr = data.split('/');
+      // show cer page
+      if (arr.length === 4 || arr.length === 3) {
+        var cerName = arr[2];
+        window.cerAttend(
+          userId + EMAIL_PREFIX,
+          userId,
+          LEARN_URL + data,
+          cerName,
+          cerName
+        );
+      } else if (arr.length === 5) {
+        // show challenge page
+        var moduleName = arr[3];
+        var challengeName = arr[4];
+        window.challengeAttend(
+          userId + EMAIL_PREFIX,
+          userId,
+          LEARN_URL + data,
+          challengeName,
+          challengeName
+        );
+        window.moduleAttend(
+          userId + EMAIL_PREFIX,
+          userId,
+          LEARN_URL + data,
+          moduleName,
+          moduleName
+        );
+      }
     }
+  } else {
   }
 
   yield call(GaTypes[type], data);
